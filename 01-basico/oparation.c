e3a0857d012d4afc7eb9ebf718552b4811b04856
julio_filizzola <juliofilizzola@hotmail.com>
2025-01-04 19:09:34 -0300

    Add basic arithmetic operations program
    Introduce a simple C program that demonstrates basic arithmetic operations: addition, subtraction, multiplication, division, and modulo. Provides an example of fundamental programming concepts for educational purposes.

---
+++ 01-basico/oparation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 5 + 5;
+    int subtraction = 5 - 5;
+    int multiplication = 5 * 5;
+    int division = 5 / 5;
+    int modulo = 5 % 5;
+    printf("Sum: %d\n", sum);
+    printf("Subtraction: %d\n", subtraction);
+    printf("Multiplication: %d\n", multiplication);
+    printf("Division: %d\n", division);
+    printf("Modulo: %d\n", modulo);
+}
