3e8215a8f27b673aa85490c53f4a36583d0bfd7b
julio_filizzola <juliofilizzola@hotmail.com>
2025-01-04 19:30:01 -0300

    Add basic math functions and modularize code structure
    Introduced `print_sum` and `print_subtraction` functions in `functions.c` and modularized `print_sum_file` in separate files (`some.c` and `some.h`). This structure improves readability and reusability by organizing code into appropriate modules.

---
+++ 01-basico/functions/some.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print_sum_file(int a, int b) {
+    int sum = a + b;
+    printf("Sum: %d\n", sum);
+}
---
+++ 01-basico/functions/some.h
@@ -0,0 +1,6 @@
+#ifndef PRINT_SUM_H
+#define PRINT_SUM_H
+
+void print_sum_file(int a, int b);
+
+#endif
